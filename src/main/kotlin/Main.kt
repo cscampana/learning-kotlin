128c3376560712d9393406d26c5944e98e473ad9
Caike Campana <csallesc@ucsd.edu>
2021-09-13 01:53:21 -0700

    More on collections and functions.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -21,26 +21,63 @@ fun main() {
     println(printNumReduced())
     printNumbers(numberToPrint = 5) // or printNumbers(5)
     stringTemplate("Caike")
-    templateSingleExpression("Caike","Campana")
+    templateSingleExpression("Caike", "Campana")
 
     /**
      * Collections and Iterations
      */
 
     // Array
-    val niceNumbers = arrayOf(2,3,4,5,6,7,8,9,10)
+    val niceNumbers = arrayOf(2, 3, 4, 5, 6, 7, 8, 9, 10)
 
     println(niceNumbers.size)
     println(niceNumbers[0])
     println(niceNumbers.get(0))
 
-    for(number in niceNumbers){
+    for (number in niceNumbers) {
         println(number)
     }
 
+    // For each structure, "it" is the default name;
+    niceNumbers.forEach {
+        println(it)
+    }
+
+    //For each using lambda
+    niceNumbers.forEach { numbers ->
+        println(numbers)
+    }
+
+    // For each with indexes
+    niceNumbers.forEachIndexed { index, numbers ->
+        println("$numbers is at index $index")
+    }
+
+    // Lists
+    val coolNumbers = listOf(1, 3, 5, 7, 9)
+    println(coolNumbers.get(0))
+    println(coolNumbers[0])
+
+    coolNumbers.forEach { number -> println(number) }
+
+    // Map                     key to value
+    val mathematicians = mapOf(1 to "Euler", 2 to "Tao", 3 to "Newton", 4 to "Ramanujan")
+
+    mathematicians.forEach { (key, value) -> println("The key is $key and the value is $value.") }
+
+    // Mutable collections
+    val coolNumbersMutable = mutableListOf(1, 3, 5, 7, 9)
+    val mathematiciansMutable =
+        mutableMapOf(1 to "Euler", 2 to "Tao", 3 to "Newton", 4 to "Ramanujan")
+
+    coolNumbersMutable.add(5)
+    mathematiciansMutable.put(key = 5, value = "Euclid")
+
+    // Function with a list in the parameters
+    collectionFunction("Hi", listOf("Euler", "Tao", "Newton", "Ramanujan"))
 }
 
-fun variables(){
+fun variables() {
     // keyword name: type = value
     // Mutable variables
     val mutable: String = "Can change"
@@ -60,11 +97,11 @@ fun variables(){
 // Declaring a top level variable
 var globalVariable: String = "Global scope"
 
-fun controlFlows(){
+fun controlFlows() {
     var greeting: String? = "Hello World"
 
     // Regular if/else statement
-    if(greeting != null){
+    if (greeting != null) {
         println(greeting)
     } else {
         println("Hi")
@@ -73,7 +110,7 @@ fun controlFlows(){
 
     var numberToBeCheck: Int? = 30
     // Just like a switch statement
-    when(numberToBeCheck){
+    when (numberToBeCheck) {
         // Cases
         3 -> println(3)
         null -> println("Null")
@@ -83,22 +120,22 @@ fun controlFlows(){
     }
 
     // if-else to set variable value
-    val validNumber = if(numberToBeCheck != null)  numberToBeCheck else "Else"
+    val validNumber = if (numberToBeCheck != null) numberToBeCheck else "Else"
 
     // when to set variable value
-    val validNumber2 = when(numberToBeCheck){
+    val validNumber2 = when (numberToBeCheck) {
         null -> "Null"
         else -> numberToBeCheck
     }
 }
 
 // fun name(parameters): return type {...}
-fun getGreeting(): String{
+fun getGreeting(): String {
     return "Hello Kotlin"
 }
 
 // Unit is a placeholder unit, means DONE!
-fun printNum(): Unit{
+fun printNum(): Unit {
     println(42)
 }
 
@@ -106,16 +143,23 @@ fun printNum(): Unit{
 fun printNumReduced() = 23
 
 // parameters: name : return type
-fun printNumbers(numberToPrint : Int){
+fun printNumbers(numberToPrint: Int) {
     println(numberToPrint)
 }
 
 // String Template, use $nameOfTheVariable to map it to a string.
-fun stringTemplate(word : String){
+fun stringTemplate(word: String) {
     val msg = "Hello $word"
     println(msg)
 }
 
 // String Template with a single expression function and multiple parameters
-fun templateSingleExpression(firstName : String, lastName : String) = println("Hi$lastName, your " +
-        "first name $firstName is awesome")
+fun templateSingleExpression(firstName: String, lastName: String) = println(
+    "Hi$lastName, your " +
+            "first name $firstName is awesome"
+)
+
+// Using collections as parameters
+fun collectionFunction(greeting: String, people: List<String>) {
+    people.forEach { person -> println("$greeting, $person") }
+}
