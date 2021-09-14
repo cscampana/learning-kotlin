2e271ae1329ab79efbd4bcdb3579c7464bfdb78a
Caike Campana <csallesc@ucsd.edu>
2021-09-13 23:40:37 -0700

    Interface implementation and inheritance.

--- src/main/kotlin/MathematicianInfo.kt
+++ src/main/kotlin/MathematicianInfo.kt
@@ -1,2 +1,34 @@
 interface MathematicianInfo {
+    // Can have properties, but cannot initialize.
+    val mathematicianInfo: String
+    // Can implement methods
+    fun printInfo(mathematician : Mathematician) {
+        println("Math")
+        mathematician.printInfo()
+    }
+}
+interface MathSessionProvide {
+    fun getMathNumber() : String
+}
+// How to implement the interface
+class ImplementedMathematician : MathematicianInfo, MathSessionProvide{
+    // You must override!
+    override val mathematicianInfo: String
+        get() = "Mathematician info"
+
+    // You must use the keyword override.
+    override fun printInfo(mathematician: Mathematician) {
+        super.printInfo(mathematician)
+        println("It works!")
+    }
+
+    override fun getMathNumber(): String {
+        return "1234"
+    }
+}
+
+fun main(){
+    val currentMathematician = ImplementedMathematician()
+    currentMathematician.printInfo(Mathematician())
+    currentMathematician.getMathNumber()
 }
